3781722758fdb2bd039d755860acbaa8e557f8dc
Ian Jones <witspr@gmail.com>
2018-11-10 10:20:57 -0600

    Added offline support

--- js/main.js
+++ js/main.js
@@ -97,3 +97,19 @@ function choose(...args) {
 function sleep(n) {
 	return new Promise(r => setTimeout(() => r(), n));
 }
+
+/// Service Worker
+// If this browser supports service workers
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('sw.js', {
+		scope: location.pathname || '/'
+	}).then(function(reg) {
+		// Registration worked
+		console.log('Registration succeeded. Scope is ', reg.scope);
+		// // Attempt to update
+		// reg.update();
+	}).catch(function(error) {
+		// Registration failed
+		console.log('Registration failed with ' + error);
+	});
+}
---
+++ sw.js
@@ -0,0 +1,56 @@
+/*
+ * Generic Service Worker - enables offline access
+ * Copyright (c) 2018 Ian Jones
+ */
+
+const CACHE_VERSION = 'beta.001';
+
+this.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(CACHE_VERSION).then(cache => {
+			return cache.addAll([
+				'https://fonts.googleapis.com/css?family=Comfortaa:400,700',
+				// 'https://fonts.googleapis.com/icon?family=Material+Icons',
+				'index.html',
+				'css/style.css',
+				'img/fixed-tile.svg',
+				'js/q.js',
+				'js/blueprint.js',
+				'js/transition.js',
+				'js/nav.js',
+				'js/grid.js',
+				'js/piece.js',
+				'js/main.js'
+			]);
+		})
+	);
+});
+
+const url_scope = location.origin.toLowerCase();
+this.addEventListener('fetch', event => {
+	if (event.request.method === 'GET') {
+		// Attempt to locate the file in the cache
+		event.respondWith(caches.open(CACHE_VERSION).then(cache => {
+			let url = event.request.url;
+			return cache.match(url).then(res => {
+				// If it's found
+				if (res) {
+					// Check for an updated version
+					fetch(event.request).then(res => {
+						cache.put(url, res);
+					});
+					// Return the cached version
+					return res.clone();
+				} else {
+					// Fetch it from the network now
+					return fetch(event.request).then(res => {
+						// Cache it
+						cache.put(url, res.clone());
+						// And return it
+						return res;
+					});
+				}
+			});
+		}));
+	}
+});
